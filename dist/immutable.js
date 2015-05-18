011cb893fa7a55d2c4c4ff5e406f0d207491329c
mgechev <mgechev@gmail.com>
2015-05-18 11:07:32 +0300

    Change in the invalid arguments handling

--- dist/immutable.js
+++ dist/immutable.js
@@ -3,10 +3,11 @@
 var immutableDirective = (function() {
   var priority = 2000;
   var scope = true;
+  var restrict = 'A';
   var link = (function(scope, el, attrs) {
     var immutable = attrs.immutable;
     if (!(/^[a-zA-Z0-9_$]+$/).test(immutable)) {
-      return ;
+      throw new Error('The "immutable" directive accepts ' + 'as argument a variable name');
     }
     if (!scope[immutable]) {
       console.warn(("No " + immutable + " property found."));
@@ -20,6 +21,7 @@ var immutableDirective = (function() {
   return {
     priority: priority,
     scope: scope,
+    restrict: restrict,
     link: link
   };
 });
