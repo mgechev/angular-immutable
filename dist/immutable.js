c4ddc0867fd4f204d7ca4bc6aa0ab50736d3cd2c
soocheng <soocheng.koh@ifastfinancial.com>
2015-11-05 20:49:38 +0800

    nested object key support, add toJS value to properties "immutable"
    added support for nested object key
    add immutables properties to directive’s scope for reading

--- dist/immutable.js
+++ dist/immutable.js
@@ -1,4 +1,4 @@
-(function () {
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.angularImmutable = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 var immutableDirective = (function() {
   var priority = 2000;
@@ -6,16 +6,13 @@ var immutableDirective = (function() {
   var restrict = 'A';
   var link = (function(scope, el, attrs) {
     var immutable = attrs.immutable;
-    if (!(/^[a-zA-Z0-9_$]+$/).test(immutable)) {
-      throw new Error('The "immutable" directive accepts ' + 'as argument a variable name');
-    }
-    if (!scope[immutable]) {
+    if (!get(immutable, scope)) {
       console.warn(("No " + immutable + " property found."));
     }
     scope.$watch((function() {
-      return scope.$parent[immutable];
+      return get(immutable, scope.$parent);
     }), (function(val) {
-      scope[immutable] = val.toJS();
+      scope.immutables = val.toJS();
     }));
   });
   return {
@@ -24,7 +21,21 @@ var immutableDirective = (function() {
     restrict: restrict,
     link: link
   };
+  function get(path, obj) {
+    var schema = obj;
+    var pList = path.split('.');
+    var len = pList.length;
+    for (var i = 0; i < len - 1; i++) {
+      var elem = pList[i];
+      if (!schema[elem])
+        schema[elem] = {};
+      schema = schema[elem];
+    }
+    return schema[pList[len - 1]];
+  }
 });
-angular.module('immutable', []).directive('immutable', immutableDirective);
+module.exports = angular.module('immutable', []).directive('immutable', immutableDirective).name;
 
-}());
+//# sourceURL=/Users/soocheng/Documents/angular-immutable/lib/immutable.js
+},{}]},{},[1])(1)
+});
