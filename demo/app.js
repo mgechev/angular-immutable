c5333881f4f40372df1ff1303ea9b81d1d1f2b9b
mgechev <mgechev@gmail.com>
2015-12-04 21:01:21 +0200

    Rollback to the previous immutable implementation

--- demo/app.js
+++ demo/app.js
@@ -18,14 +18,6 @@ function SampleCtrl($scope, $timeout) {
   var TOTAL = 100;
   var start = Date.now();
   $scope.list = Immutable.List(buildCollection(SIZE));
-  $scope.$watchCollection('list', function () {
-  });
-  $scope.$watchCollection('list', function () {
-  });
-  $scope.$watchCollection('list', function () {
-  });
-  $scope.$watchCollection('list', function () {
-  });
   function changeCollection() {
     if (runs >= TOTAL) {
       console.log('%cDone!',
@@ -38,58 +30,12 @@ function SampleCtrl($scope, $timeout) {
     $timeout(function () {
       var idx = Math.random() * SIZE - 1;
       $scope.list = $scope.list.set(idx, Math.random());
-
-//      if (runs % 2) {
-//        $scope.list = $scope.list.push(Math.random());
-//      } else {
-//        $scope.list = $scope.list.pop();
-//      }
       runs += 1;
       changeCollection();
     }, 0);
   }
   changeCollection();
 }
-
-function SampleCtrl2($scope, $timeout) {
-  'use strict';
-  var runs = 0;
-  var SIZE = 50000;
-  var TOTAL = 100;
-  var start = Date.now();
-  $scope.$watchCollection('list', function () {
-  });
-  $scope.$watchCollection('list', function () {
-  });
-  $scope.$watchCollection('list', function () {
-  });
-  $scope.$watchCollection('list', function () {
-  });
-  $scope.list = buildCollection(SIZE);
-  function changeCollection() {
-    if (runs >= TOTAL) {
-      console.log('%cDone!',
-          'font-size: 50px; color: blue;' +
-          'font-weight: bold; font-family: impact;');
-      console.log('%c' + ((Date.now() - start) / 1000) +
-          ' seconds required.', 'font-size: 30px; color: red;');
-      return;
-    }
-    $timeout(function () {
-      var idx = Math.round(Math.random() * SIZE) - 1;
-      $scope.list[idx] = Math.random();
-
-//      if (runs % 2) {
-//        $scope.list = $scope.list.push(Math.random());
-//      } else {
-//        $scope.list = $scope.list.pop();
-//      }
-      runs += 1;
-      changeCollection();
-    }, 0);
-  }
-  changeCollection();
-}
-
 
 sampleApp.controller('SampleCtrl', SampleCtrl);
+
--- dist/immutable.js
+++ dist/immutable.js
@@ -1,41 +1,31 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.angularImmutable = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
-var immutableDirective = (function() {
+'use strict';
+
+/* global angular */
+
+var immutableDirective = function immutableDirective() {
   var priority = 2000;
   var scope = true;
   var restrict = 'A';
-  var link = (function(scope, el, attrs) {
+  var link = function link(scope, el, attrs) {
     var immutable = attrs.immutable;
-    if (!get(immutable, scope)) {
-      console.warn(("No " + immutable + " property found."));
+
+    if (!/^[a-zA-Z0-9_$]+$/.test(immutable)) {
+      throw new Error('The "immutable" directive accepts ' + 'as argument a variable name');
     }
-    scope.$watch((function() {
-      return get(immutable, scope.$parent);
-    }), (function(val) {
-      scope.immutables = val.toJS();
-    }));
-  });
-  return {
-    priority: priority,
-    scope: scope,
-    restrict: restrict,
-    link: link
+    if (!scope[immutable]) {
+      console.warn('No ' + immutable + ' property found.');
+    }
+    scope.$watch(function () {
+      return scope.$parent[immutable];
+    }, function (val) {
+      scope[immutable] = val.toJS();
+    });
   };
-  function get(path, obj) {
-    var schema = obj;
-    var pList = path.split('.');
-    var len = pList.length;
-    for (var i = 0; i < len - 1; i++) {
-      var elem = pList[i];
-      if (!schema[elem])
-        schema[elem] = {};
-      schema = schema[elem];
-    }
-    return schema[pList[len - 1]];
-  }
-});
+  return { priority: priority, scope: scope, restrict: restrict, link: link };
+};
+
 module.exports = angular.module('immutable', []).directive('immutable', immutableDirective).name;
 
-//# sourceURL=/Users/soocheng/Documents/angular-immutable/lib/immutable.js
 },{}]},{},[1])(1)
 });
